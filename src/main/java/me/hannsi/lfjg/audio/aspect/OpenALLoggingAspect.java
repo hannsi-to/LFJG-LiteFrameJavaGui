d2c643446819cf7c04eabf176f9f4d4908bfe799
hannsi-to <hannsi.to2@gmail.com>
2025-04-01 04:11:49 +0900

    ファイルダイアログを追加

---
+++ src/main/java/me/hannsi/lfjg/audio/aspect/OpenALLoggingAspect.java
@@ -0,0 +1,18 @@
+package me.hannsi.lfjg.audio.aspect;
+
+import me.hannsi.lfjg.audio.openAL.OpenALDebug;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class OpenALLoggingAspect {
+    @After("execution(* org.lwjgl.openal.AL10.*(..)) || execution(* org.lwjgl.openal.AL11.*(..))")
+    public void checkAL(JoinPoint joinPoint) {
+        if (joinPoint.getSignature().getName().equals("alGetError")) {
+            return;
+        }
+
+        OpenALDebug.getOpenALError("checkAL");
+    }
+}
