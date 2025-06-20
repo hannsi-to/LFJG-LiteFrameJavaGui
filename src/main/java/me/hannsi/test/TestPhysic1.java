cda5975eb62c850329ddc1f53289ed2d62201e37
hannsi-to <hannsi.to2@gmail.com>
2025-06-20 23:03:00 +0900

    忘れてた

---
+++ src/main/java/me/hannsi/test/TestPhysic1.java
@@ -0,0 +1,62 @@
+package me.hannsi.test;
+
+import me.hannsi.lfjg.frame.Frame;
+import me.hannsi.lfjg.physic.PhysicWorld;
+import me.hannsi.lfjg.physic.RigidBody;
+import me.hannsi.lfjg.render.renderers.polygon.GLRect;
+import me.hannsi.lfjg.render.system.scene.IScene;
+import me.hannsi.lfjg.render.system.scene.Scene;
+import me.hannsi.lfjg.utils.graphics.color.Color;
+
+public class TestPhysic1 implements IScene {
+    private final Scene scene;
+    private final Frame frame;
+
+    private PhysicWorld physicWorld;
+    private GLRect glGround;
+    private GLRect glRect;
+
+    public TestPhysic1(Frame frame) {
+        this.frame = frame;
+        scene = new Scene("TestPhysic1", this);
+    }
+
+    @Override
+    public void init() {
+        glGround = new GLRect("Ground");
+        glGround.rectWH(0, 0, frame.getFrameBufferWidth(), 10, Color.of(255, 255, 255, 255));
+
+        glRect = new GLRect("Rect");
+        glRect.rectWH(600, 600, 100, 100, Color.of(255, 255, 255, 255));
+
+        physicWorld = PhysicWorld.initPhysicWorld()
+                .addRigidBody(
+                        RigidBody.createRigidBody()
+                                .setMass(1)
+                                .setNoGravity(true)
+                                .attachGLObject(glRect)
+                )
+                .addRigidBody(
+                        RigidBody.createRigidBody()
+                                .setMass(1)
+                                .attachGLObject(glRect)
+                );
+    }
+
+    @Override
+    public void drawFrame() {
+        glGround.draw();
+        glRect.draw();
+        physicWorld.simulate(1f / frame.getFps());
+    }
+
+    @Override
+    public void stopFrame() {
+
+    }
+
+    @Override
+    public Scene getScene() {
+        return scene;
+    }
+}
