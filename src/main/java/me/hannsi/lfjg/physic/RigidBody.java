d1b9e9d7b7cf7f9ddc625a58059599e6e79b6522
hannsi-to <hannsi.to2@gmail.com>
2025-06-20 23:02:05 +0900

    Physic ちょっと書き直し

---
+++ src/main/java/me/hannsi/lfjg/physic/RigidBody.java
@@ -0,0 +1,51 @@
+package me.hannsi.lfjg.physic;
+
+import me.hannsi.lfjg.render.renderers.GLObject;
+import org.joml.Vector2f;
+
+public class RigidBody {
+    public GLObject glObject;
+    public float mass;
+    public boolean noGravity;
+
+    public Vector2f velocity;
+    public Vector2f acceleration;
+
+    RigidBody() {
+        this.mass = 1;
+        this.noGravity = false;
+
+        this.velocity = new Vector2f();
+        this.acceleration = new Vector2f();
+    }
+
+    public static RigidBody createRigidBody() {
+        return new RigidBody();
+    }
+
+    public RigidBody setMass(float mass) {
+        this.mass = mass;
+        return this;
+    }
+
+    public RigidBody setNoGravity(boolean noGravity) {
+        this.noGravity = noGravity;
+        return this;
+    }
+
+    public RigidBody attachGLObject(GLObject glObject) {
+        this.glObject = glObject;
+        return this;
+    }
+
+    public void applyForce(Vector2f force) {
+        acceleration.add(force.x() / mass, force.y() / mass);
+    }
+
+    public void integrate(float dt, float scale) {
+        velocity.add(acceleration.mul(dt, new Vector2f()));
+        Vector2f deltaMove = velocity.mul(dt, new Vector2f());
+        glObject.translate(deltaMove.x() * scale, deltaMove.y() * scale, 0);
+        acceleration = new Vector2f();
+    }
+}
