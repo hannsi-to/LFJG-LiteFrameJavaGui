f13bb8ccf6540abf7e755964b0d19777a06dfb58
hannsi-to <hannsi.to2@gmail.com>
2025-08-29 06:42:16 +0900

    BoxBlur追加

---
+++ render/src/main/java/me/hannsi/lfjg/render/effect/effects/BoxBlur.java
@@ -0,0 +1,40 @@
+package me.hannsi.lfjg.render.effect.effects;
+
+import me.hannsi.lfjg.render.effect.system.EffectBase;
+import me.hannsi.lfjg.render.system.rendering.FrameBuffer;
+import me.hannsi.lfjg.render.system.shader.FragmentShaderType;
+import me.hannsi.lfjg.render.system.shader.UploadUniformType;
+
+import static me.hannsi.lfjg.render.LFJGRenderContext.shaderProgram;
+
+public class BoxBlur extends EffectBase {
+    private int kernelX = 10;
+    private int kernelY = 10;
+
+    BoxBlur(String name) {
+        super(name, false);
+    }
+
+    public static BoxBlur createBoxBlur(String name) {
+        return new BoxBlur(name);
+    }
+
+    public BoxBlur kernelX(int kernelX) {
+        this.kernelX = kernelX;
+        return this;
+    }
+
+    public BoxBlur kernelY(int kernelY) {
+        this.kernelY = kernelY;
+        return this;
+    }
+
+    @Override
+    public void drawFrameBuffer(FrameBuffer latestFrameBuffer) {
+        shaderProgram.setUniform("fragmentShaderType", UploadUniformType.PER_FRAME, FragmentShaderType.BOX_BLUR.getId());
+        shaderProgram.setUniform("kernelX", UploadUniformType.ON_CHANGE, kernelX);
+        shaderProgram.setUniform("kernelY", UploadUniformType.ON_CHANGE, kernelY);
+
+        super.drawFrameBuffer(latestFrameBuffer);
+    }
+}
