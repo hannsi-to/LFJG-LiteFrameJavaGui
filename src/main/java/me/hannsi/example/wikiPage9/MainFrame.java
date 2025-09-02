a60e11e8b7e81e4dbf3777b447c5c66e5a457bff
hannsi-to <hannsi.to2@gmail.com>
2025-09-02 23:20:41 +0900

    wiki

---
+++ src/main/java/me/hannsi/example/wikiPage9/MainFrame.java
@@ -0,0 +1,65 @@
+package me.hannsi.example.wikiPage9;
+
+import me.hannsi.lfjg.core.utils.graphics.color.Color;
+import me.hannsi.lfjg.core.utils.reflection.location.Location;
+import me.hannsi.lfjg.core.utils.toolkit.UnicodeBlocks;
+import me.hannsi.lfjg.frame.Frame;
+import me.hannsi.lfjg.frame.system.LFJGFrame;
+import me.hannsi.lfjg.render.renderers.text.GLText;
+import me.hannsi.lfjg.render.system.text.AlignType;
+import me.hannsi.lfjg.render.system.text.font.Font;
+
+import static me.hannsi.lfjg.render.LFJGRenderTextContext.fontCache;
+
+public class MainFrame implements LFJGFrame {
+    GLText glText;
+    private Frame frame;
+
+    public static void main(String[] args) {
+        new MainFrame().setFrame();
+    }
+
+    @Override
+    public void init() {
+        frame.updateLFJGLContext();
+
+        fontCache.createCache(
+                "Font1",
+                Font.createCustomFont()
+                        .name("Font1")
+                        .trueTypeFontPath(Location.fromFile("C:\\Windows\\Fonts\\Arial.ttf"))
+                        .unicodeBlocks(UnicodeBlocks.getBlocks("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.:,;'\"(!?)+-*/="))
+                        .createCache()
+        );
+
+        glText = new GLText("GLText1");
+        glText.text(
+                "Font1",
+                "Hello World!!",
+                500, 500,
+                64,
+                Color.WHITE,
+                true,
+                AlignType.LEFT_BASELINE
+        );
+    }
+
+    @Override
+    public void drawFrame() {
+        glText.draw();
+    }
+
+    @Override
+    public void stopFrame() {
+        glText.cleanup();
+    }
+
+    @Override
+    public void setFrameSetting() {
+
+    }
+
+    public void setFrame() {
+        frame = new Frame(this, "MainFrame");
+    }
+}
