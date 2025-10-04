77df8e5ede21e7c7edd7e0d646f0b4952f71805a
hannsi-to <hannsi.to2@gmail.com>
2025-10-05 05:15:16 +0900

    削除

--- render/src/main/java/me/hannsi/lfjg/render/system/mesh/DrawElementsIndirectCommand.java
+++ render/src/main/java/me/hannsi/lfjg/render/system/mesh/DrawElementsIndirectCommand.java
@@ -1,8 +1,5 @@
 package me.hannsi.lfjg.render.system.mesh;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-
 public class DrawElementsIndirectCommand {
     public static final int COMMAND_COUNT = 5;
     public static final int BYTES = COMMAND_COUNT * Integer.BYTES;
@@ -19,14 +16,5 @@ public class DrawElementsIndirectCommand {
         this.firstIndex = firstIndex;
         this.baseVertex = baseVertex;
         this.baseInstance = baseInstance;
-    }
-
-    public void writeToByteBuffer(ByteBuffer buffer) {
-        buffer.order(ByteOrder.nativeOrder());
-        buffer.putInt(count);
-        buffer.putInt(instanceCount);
-        buffer.putInt(firstIndex);
-        buffer.putInt(baseVertex);
-        buffer.putInt(baseInstance);
     }
 }
