9f392c901c58ced09b68406fbcbc2dd2905048d8
hannsi-to <hannsi.to2@gmail.com>
2025-10-18 16:31:50 +0900

    さーくじょ

--- render/src/main/java/me/hannsi/lfjg/render/system/mesh/BufferObjectType.java
+++ render/src/main/java/me/hannsi/lfjg/render/system/mesh/BufferObjectType.java
@@ -2,15 +2,12 @@ package me.hannsi.lfjg.render.system.mesh;
 
 import me.hannsi.lfjg.core.utils.type.system.IEnumTypeBase;
 import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL40;
 
 public enum BufferObjectType implements IEnumTypeBase {
     POSITION_BUFFER(0, GL15.GL_ARRAY_BUFFER, 0, 3, "PositionBuffer"),
     COLOR_BUFFER(1, GL15.GL_ARRAY_BUFFER, 1, 4, "ColorBuffer"),
     TEXTURE_BUFFER(2, GL15.GL_ARRAY_BUFFER, 2, 2, "TextureBuffer"),
-    NORMAL_BUFFER(3, GL15.GL_ARRAY_BUFFER, 3, 3, "NormalBuffer"),
-    INDIRECT_BUFFER(4, GL40.GL_DRAW_INDIRECT_BUFFER, -1, -1, "IndirectBuffer"),
-    ELEMENT_ARRAY_BUFFER(5, GL15.GL_ELEMENT_ARRAY_BUFFER, -1, -1, "ElementArrayBuffer");
+    NORMAL_BUFFER(3, GL15.GL_ARRAY_BUFFER, 3, 3, "NormalBuffer");
 
     final int id;
     final int glId;
