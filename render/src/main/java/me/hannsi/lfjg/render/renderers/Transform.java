a56591a87df2cd0b5b096032222ebc8910e8bd9b
hannsi-to <hannsi.to2@gmail.com>
2025-12-22 03:07:46 +0900

    Transform 大幅変更

--- render/src/main/java/me/hannsi/lfjg/render/renderers/Transform.java
+++ render/src/main/java/me/hannsi/lfjg/render/renderers/Transform.java
@@ -1,59 +1,83 @@
 package me.hannsi.lfjg.render.renderers;
 
+import me.hannsi.lfjg.core.utils.type.types.ProjectionType;
 import org.joml.Matrix4f;
+import org.joml.Quaternionf;
+
+import static me.hannsi.lfjg.core.Core.UNSAFE;
+import static me.hannsi.lfjg.render.LFJGRenderContext.MAIN_CAMERA;
 
 public class Transform {
-    private final GLObject glObject;
-    private final Matrix4f modelMatrix;
-
+    public static final int BYTES = 80;
+    protected final Matrix4f TEMP_MATRIX = new Matrix4f();
+    protected final Matrix4f MODEL_MATRIX = new Matrix4f();
+    private final Quaternionf rotation;
+    private final ProjectionType projectionType;
+    protected boolean dirtyFlag = true;
+    private int layer;
+    private long address;
     private float x;
     private float y;
     private float z;
-
-    private float angleX;
-    private float angleY;
-    private float angleZ;
-
     private float scaleX;
     private float scaleY;
     private float scaleZ;
 
-    public Transform(GLObject glObject) {
-        this(new Matrix4f(), glObject);
+    public Transform(int layer) {
+        this(0, 0, 0, 0, 0, 0, 1, 1, 1, ProjectionType.ORTHOGRAPHIC_PROJECTION, layer);
     }
 
-    public Transform(Matrix4f modelMatrix, GLObject glObject) {
-        this.glObject = glObject;
-        this.modelMatrix = modelMatrix;
+    public Transform(float x, float y, float z, float angleX, float angleY, float angleZ, float scaleX, float scaleY, float scaleZ, ProjectionType projectionType, int layer) {
+        this(x, y, z, new Quaternionf().rotateX(angleX).rotateY(angleY).rotateZ(angleZ), scaleX, scaleY, scaleZ, projectionType, layer);
     }
 
-    public Transform(GLObject glObject, Matrix4f modelMatrix, float x, float y, float z, float angleX, float angleY, float angleZ, float scaleX, float scaleY, float scaleZ) {
-        this.glObject = glObject;
-        this.modelMatrix = modelMatrix;
+    public Transform(float x, float y, float z, Quaternionf rotation, float scaleX, float scaleY, float scaleZ, ProjectionType projectionType, int layer) {
         this.x = x;
         this.y = y;
         this.z = z;
-        this.angleX = angleX;
-        this.angleY = angleY;
-        this.angleZ = angleZ;
+        this.rotation = rotation;
         this.scaleX = scaleX;
         this.scaleY = scaleY;
         this.scaleZ = scaleZ;
+        this.projectionType = projectionType;
+        this.layer = layer;
+    }
+
+    public Transform getToAddress(long address, Matrix4f vpMatrix) {
+        if (!dirtyFlag && !MAIN_CAMERA.isDirtyFlag()) {
+            return this;
+        }
+
+        this.address = address;
+        if (dirtyFlag) {
+            MODEL_MATRIX.translationRotateScale(x, y, z, rotation.x, rotation.y, rotation.z, rotation.w, scaleX, scaleY, scaleZ);
+            dirtyFlag = false;
+        }
+
+        vpMatrix.mul(MODEL_MATRIX, TEMP_MATRIX);
+
+        TEMP_MATRIX.getToAddress(address);
+        UNSAFE.putInt(address + 64, layer);
+
+        MAIN_CAMERA.setDirtyFlag(false);
+
+        return this;
     }
 
     public Transform newInstance() {
-        return new Transform(glObject, modelMatrix, x, y, z, angleX, angleY, angleZ, scaleX, scaleY, scaleZ);
+        return new Transform(x, y, z, new Quaternionf(rotation), scaleX, scaleY, scaleZ, projectionType, layer);
     }
 
     public Transform reset() {
-        this.angleX = 0;
-        this.angleY = 0;
-        this.angleZ = 0;
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.rotation.identity();
         this.scaleX = 1;
         this.scaleY = 1;
         this.scaleZ = 1;
 
-        modelMatrix.identity();
+        dirtyFlag = true;
 
         return this;
     }
@@ -62,35 +86,31 @@ public class Transform {
         this.x += x;
         this.y += y;
         this.z += z;
-        modelMatrix.translate(x, y, z);
+
+        dirtyFlag = true;
 
         return this;
     }
 
     public Transform rotateXYZ(float angleX, float angleY, float angleZ) {
-        this.angleX += angleX;
-        this.angleY += angleY;
-        this.angleZ += angleZ;
-        modelMatrix.rotateXYZ(angleX, angleY, angleZ);
+        rotation
+                .rotateX(angleX)
+                .rotateY(angleY)
+                .rotateZ(angleZ);
+
+        dirtyFlag = true;
 
         return this;
     }
 
     public Transform scale(float scaleX, float scaleY, float scaleZ) {
-        this.scaleX += scaleX;
-        this.scaleY += scaleY;
-        this.scaleZ += scaleZ;
-        modelMatrix.scale(scaleX, scaleY, scaleZ);
+        this.scaleX *= scaleX;
+        this.scaleY *= scaleY;
+        this.scaleZ *= scaleZ;
+
+        dirtyFlag = true;
 
         return this;
-    }
-
-    public GLObject getGlObject() {
-        return glObject;
-    }
-
-    public Matrix4f getModelMatrix() {
-        return modelMatrix;
     }
 
     public float getX() {
@@ -99,6 +119,8 @@ public class Transform {
 
     public void setX(float x) {
         this.x = x;
+
+        dirtyFlag = true;
     }
 
     public float getY() {
@@ -107,6 +129,8 @@ public class Transform {
 
     public void setY(float y) {
         this.y = y;
+
+        dirtyFlag = true;
     }
 
     public float getZ() {
@@ -115,30 +139,16 @@ public class Transform {
 
     public void setZ(float z) {
         this.z = z;
+
+        dirtyFlag = true;
     }
 
-    public float getAngleX() {
-        return angleX;
+    public Quaternionf getRotation() {
+        return rotation;
     }
 
-    public void setAngleX(float angleX) {
-        this.angleX = angleX;
-    }
-
-    public float getAngleY() {
-        return angleY;
-    }
-
-    public void setAngleY(float angleY) {
-        this.angleY = angleY;
-    }
-
-    public float getAngleZ() {
-        return angleZ;
-    }
-
-    public void setAngleZ(float angleZ) {
-        this.angleZ = angleZ;
+    public long getAddress() {
+        return address;
     }
 
     public float getScaleX() {
@@ -147,6 +157,8 @@ public class Transform {
 
     public void setScaleX(float scaleX) {
         this.scaleX = scaleX;
+
+        dirtyFlag = true;
     }
 
     public float getScaleY() {
@@ -155,6 +167,8 @@ public class Transform {
 
     public void setScaleY(float scaleY) {
         this.scaleY = scaleY;
+
+        dirtyFlag = true;
     }
 
     public float getScaleZ() {
@@ -163,5 +177,19 @@ public class Transform {
 
     public void setScaleZ(float scaleZ) {
         this.scaleZ = scaleZ;
+
+        dirtyFlag = true;
+    }
+
+    public ProjectionType getProjectionType() {
+        return projectionType;
+    }
+
+    public int getLayer() {
+        return layer;
+    }
+
+    public void setLayer(int layer) {
+        this.layer = layer;
     }
 }
