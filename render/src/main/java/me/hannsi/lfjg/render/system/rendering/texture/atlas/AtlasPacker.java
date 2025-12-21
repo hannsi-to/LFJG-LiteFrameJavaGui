3e8afbbd008df6118a3116bfb6d3257955ff7209
hannsi-to <hannsi.to2@gmail.com>
2025-12-22 02:51:41 +0900

    PERSISTENT_MAPPED_SSBO に変更

--- render/src/main/java/me/hannsi/lfjg/render/system/rendering/texture/atlas/AtlasPacker.java
+++ render/src/main/java/me/hannsi/lfjg/render/system/rendering/texture/atlas/AtlasPacker.java
@@ -1,27 +1,22 @@
 package me.hannsi.lfjg.render.system.rendering.texture.atlas;
 
 import me.hannsi.lfjg.core.debug.DebugLog;
-import me.hannsi.lfjg.render.system.mesh.MeshConstants;
-import me.hannsi.lfjg.render.system.mesh.persistent.TestPersistentMappedSSBO;
 import org.lwjgl.BufferUtils;
 
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
+import static me.hannsi.lfjg.render.LFJGRenderContext.PERSISTENT_MAPPED_SSBO;
+import static me.hannsi.lfjg.render.system.mesh.InstanceData.NO_ATTACH_TEXTURE;
 import static org.lwjgl.opengl.GL11.GL_MAX_TEXTURE_SIZE;
 import static org.lwjgl.opengl.GL11.glGetInteger;
-import static org.lwjgl.opengl.GL30.glBindBufferRange;
-import static org.lwjgl.opengl.GL43.GL_SHADER_STORAGE_BUFFER;
 
 public class AtlasPacker {
-    public static final int NO_ATTACH_TEXTURE = -1;
-
     private final int atlasWidth;
     private final int atlasHeight;
     private final ByteBuffer atlasBuffer;
     private final Map<String, Sprite> sprites;
-    protected TestPersistentMappedSSBO persistentMappedSSBO;
     private int currentX;
     private int currentY;
     private int rowHeight;
@@ -48,8 +43,6 @@ public class AtlasPacker {
         this.currentY = currentY;
         this.rowHeight = rowHeight;
         this.atlasBuffer = BufferUtils.createByteBuffer(atlasWidth * atlasHeight * 4);
-
-        persistentMappedSSBO = new TestPersistentMappedSSBO(MeshConstants.DEFAULT_FLAGS_HINT, 1000);
     }
 
     public AtlasPacker addSprite(String name, Sprite sprite) {
@@ -93,7 +86,7 @@ public class AtlasPacker {
             }
         }
 
-        persistentMappedSSBO.addInt(1, sprites.size()).addInt(1, 0).addInt(1, 0).addInt(1, 0);
+        PERSISTENT_MAPPED_SSBO.addInt(1, sprites.size()).addInt(1, 0).addInt(1, 0).addInt(1, 0);
         final float INSET = 0.5f;
         for (Map.Entry<String, Sprite> spriteEntry : sprites.entrySet()) {
             Sprite sprite = spriteEntry.getValue();
@@ -106,23 +99,22 @@ public class AtlasPacker {
             float uv_width_inset = (sprite.width - (2 * INSET)) / atlasWidth;
             float uv_height_inset = (sprite.height - (2 * INSET)) / atlasHeight;
 
-            persistentMappedSSBO.addVec4(1, uvX, uvY, uv_width_inset, uv_height_inset);
+            PERSISTENT_MAPPED_SSBO.addVec4(1, uvX, uvY, uv_width_inset, uv_height_inset);
         }
 
-        persistentMappedSSBO.addInt(2, 2);
-        persistentMappedSSBO.addInt(2, 0);
-        persistentMappedSSBO.addInt(2, 2);
-        persistentMappedSSBO.addInt(2, 3);
-
-        for (Map.Entry<Integer, TestPersistentMappedSSBO.SSBOBindingData> bindingDataEntry : persistentMappedSSBO.getBindingDatum().entrySet()) {
-            TestPersistentMappedSSBO.SSBOBindingData ssboBindingData = bindingDataEntry.getValue();
-
-            glBindBufferRange(GL_SHADER_STORAGE_BUFFER, bindingDataEntry.getKey(), persistentMappedSSBO.getBufferId(), ssboBindingData.offset, ssboBindingData.size);
-        }
-
-        persistentMappedSSBO.syncToGPU();
+        PERSISTENT_MAPPED_SSBO.syncToGPU();
 
         return atlasBuffer;
+    }
+
+    public int getLayer(String name) {
+        if (!sprites.containsKey(name)) {
+            DebugLog.error(getClass(), "The sprite registered under " + name + " does not exist.");
+
+            return NO_ATTACH_TEXTURE;
+        }
+
+        return sprites.get(name).instanceLayer;
     }
 
     public int getAtlasWidth() {
