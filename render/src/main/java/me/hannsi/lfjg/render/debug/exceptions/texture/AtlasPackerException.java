3142fee2b311125eb754ccf458dd4d65ef9271c7
hannsi-to <hannsi.to2@gmail.com>
2025-12-24 02:52:27 +0900

    AtlasPackerExceptionを追加

---
+++ render/src/main/java/me/hannsi/lfjg/render/debug/exceptions/texture/AtlasPackerException.java
@@ -0,0 +1,7 @@
+package me.hannsi.lfjg.render.debug.exceptions.texture;
+
+public class AtlasPackerException extends RuntimeException {
+    public AtlasPackerException(String message) {
+        super(message);
+    }
+}
