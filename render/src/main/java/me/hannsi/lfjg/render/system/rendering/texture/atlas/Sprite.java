168ab1230cc81d4898ed17246224f9d6c0e32ea0
hannsi-to <hannsi.to2@gmail.com>
2025-12-25 06:28:01 +0900

    Spriteを変更

--- render/src/main/java/me/hannsi/lfjg/render/system/rendering/texture/atlas/Sprite.java
+++ render/src/main/java/me/hannsi/lfjg/render/system/rendering/texture/atlas/Sprite.java
@@ -5,22 +5,22 @@ import org.lwjgl.BufferUtils;
 import java.nio.ByteBuffer;
 
 public class Sprite {
-    public final int instanceLayer;
     public final int width;
     public final int height;
     public final ByteBuffer data;
+    public boolean commited;
     public int offsetX;
     public int offsetY;
     public int offsetZ;
 
-    public Sprite(int instanceLayer, int width, int height, ByteBuffer data) {
-        this.instanceLayer = instanceLayer;
+    public Sprite(int width, int height, ByteBuffer data) {
         this.width = width;
         this.height = height;
         this.data = data;
+        this.commited = false;
     }
 
-    public static Sprite createRandomColor(int instanceLayer, int width, int height, boolean fixedColor) {
+    public static Sprite createRandomColor(int width, int height) {
         ByteBuffer buffer = BufferUtils.createByteBuffer(width * height * 4);
 
         byte[][] fixedColors = {
@@ -35,21 +35,21 @@ public class Sprite {
         byte b;
         byte a = (byte) 255;
 
-        if (instanceLayer >= 0 && instanceLayer <= 3 && fixedColor) {
-            r = fixedColors[instanceLayer][0];
-            g = fixedColors[instanceLayer][1];
-            b = fixedColors[instanceLayer][2];
-        } else {
-            r = (byte) (Math.random() * 255);
-            g = (byte) (Math.random() * 255);
-            b = (byte) (Math.random() * 255);
-        }
+        r = (byte) (Math.random() * 255);
+        g = (byte) (Math.random() * 255);
+        b = (byte) (Math.random() * 255);
 
         for (int i = 0; i < width * height; i++) {
             buffer.put(r).put(g).put(b).put(a);
         }
         buffer.flip();
 
-        return new Sprite(instanceLayer, width, height, buffer);
+        return new Sprite(width, height, buffer);
+    }
+
+    public Sprite setCommited(boolean commited) {
+        this.commited = commited;
+
+        return this;
     }
 }
