bb6af1ab292f52c89808ff5d4638f22b448a72f8
hannsi-to <hannsi.to2@gmail.com>
2025-12-26 04:26:57 +0900

    SparseTexture2DArrayException を追加

---
+++ render/src/main/java/me/hannsi/lfjg/render/debug/exceptions/texture/SparseTexture2DArrayException.java
@@ -0,0 +1,7 @@
+package me.hannsi.lfjg.render.debug.exceptions.texture;
+
+public class SparseTexture2DArrayException extends RuntimeException {
+    public SparseTexture2DArrayException(String message) {
+        super(message);
+    }
+}
