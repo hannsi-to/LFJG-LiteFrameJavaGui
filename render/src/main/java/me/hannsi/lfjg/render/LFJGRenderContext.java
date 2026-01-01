b5841c2d51c903d4d4012575b2d6ab6938f1bd73
hannsi-to <hannsi.to2@gmail.com>
2026-01-01 11:37:13 +0900

    LFJGRenderContext RENAME

--- render/src/main/java/me/hannsi/lfjg/render/LFJGRenderContext.java
+++ render/src/main/java/me/hannsi/lfjg/render/LFJGRenderContext.java
@@ -8,9 +8,9 @@ import me.hannsi.lfjg.render.system.mesh.TestMesh;
 import me.hannsi.lfjg.render.system.mesh.persistent.*;
 import me.hannsi.lfjg.render.system.rendering.GLStateCache;
 import me.hannsi.lfjg.render.system.rendering.VAORendering;
+import me.hannsi.lfjg.render.system.rendering.texture.atlas.AtlasPacker;
 import me.hannsi.lfjg.render.system.shader.ShaderProgram;
 import me.hannsi.lfjg.render.uitl.id.GLObjectPool;
-import me.hannsi.lfjg.render.uitl.id.IdPool;
 import org.joml.Matrix4f;
 
 import static me.hannsi.lfjg.core.Core.projection2D;
@@ -18,40 +18,44 @@ import static me.hannsi.lfjg.core.Core.projection3D;
 import static me.hannsi.lfjg.core.SystemSetting.*;
 import static org.lwjgl.opengl.ARBSparseTexture.GL_VIRTUAL_PAGE_SIZE_X_ARB;
 import static org.lwjgl.opengl.ARBSparseTexture.GL_VIRTUAL_PAGE_SIZE_Y_ARB;
-import static org.lwjgl.opengl.GL11.GL_RGBA8;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL30.GL_MAX_ARRAY_TEXTURE_LAYERS;
 import static org.lwjgl.opengl.GL30.GL_TEXTURE_2D_ARRAY;
 import static org.lwjgl.opengl.GL42.glGetInternalformativ;
 
 public class LFJGRenderContext {
-    public static final int PAGE_SIZE_X;
-    public static final int PAGE_SIZE_Y;
-    public static IdPool ID_POOL;
+    public static final int VIRTUAL_PAGE_SIZE_X;
+    public static final int VIRTUAL_PAGE_SIZE_Y;
+    public static final int MAX_ARRAY_TEXTURE_LAYERS;
+    public static final int MAX_TEXTURE_SIZE;
     public static GLObjectPool GL_OBJECT_POOL;
     public static Camera MAIN_CAMERA;
     public static Matrix4f precomputedViewProjection2D;
     public static Matrix4f precomputedViewProjection3D;
-    public static ShaderProgram SHADER_PROGRAM;
-    public static TestPersistentMappedVBO PERSISTENT_MAPPED_VBO;
-    public static TestPersistentMappedEBO PERSISTENT_MAPPED_EBO;
-    public static TestPersistentMappedIBO PERSISTENT_MAPPED_IBO;
-    public static TestPersistentMappedSSBO PERSISTENT_MAPPED_SSBO;
-    public static TestPersistentMappedPBO PERSISTENT_MAPPED_PBO;
-    public static TestMesh MESH;
-    public static VAORendering VAO_RENDERING;
-    public static GLStateCache GL_STATE_CACHE;
-    public static TextureCache TEXTURE_CACHE;
+    public static GLStateCache glStateCache;
+    public static AtlasPacker atlasPacker;
+    public static ShaderProgram shaderProgram;
+    public static TestPersistentMappedVBO persistentMappedVBO;
+    public static TestPersistentMappedEBO persistentMappedEBO;
+    public static TestPersistentMappedIBO persistentMappedIBO;
+    public static TestPersistentMappedSSBO persistentMappedSSBO;
+    public static TestPersistentMappedPBO persistentMappedPBO;
+    public static TestMesh mesh;
+    public static VAORendering vaoRendering;
+    public static TextureCache textureCache;
 
     static {
         int[] x = new int[1];
         int[] y = new int[1];
         glGetInternalformativ(GL_TEXTURE_2D_ARRAY, GL_RGBA8, GL_VIRTUAL_PAGE_SIZE_X_ARB, x);
         glGetInternalformativ(GL_TEXTURE_2D_ARRAY, GL_RGBA8, GL_VIRTUAL_PAGE_SIZE_Y_ARB, y);
-        PAGE_SIZE_X = x[0];
-        PAGE_SIZE_Y = y[0];
+        VIRTUAL_PAGE_SIZE_X = x[0];
+        VIRTUAL_PAGE_SIZE_Y = y[0];
+        MAX_ARRAY_TEXTURE_LAYERS = glGetInteger(GL_MAX_ARRAY_TEXTURE_LAYERS);
+        MAX_TEXTURE_SIZE = glGetInteger(GL_MAX_TEXTURE_SIZE);
     }
 
     public static void init() {
-        ID_POOL = new IdPool();
         GL_OBJECT_POOL = new GLObjectPool();
 
         MAIN_CAMERA = new Camera();
@@ -59,29 +63,29 @@ public class LFJGRenderContext {
         precomputedViewProjection2D = projection2D.getMatrix4f().mul(MAIN_CAMERA.getViewMatrix());
         precomputedViewProjection3D = projection3D.getMatrix4f().mul(MAIN_CAMERA.getViewMatrix());
 
-        GL_STATE_CACHE = new GLStateCache();
+        glStateCache = new GLStateCache();
 
-        SHADER_PROGRAM = new ShaderProgram();
-        SHADER_PROGRAM.createVertexShader(Location.fromResource("shader/VertexShader.vsh"));
-        SHADER_PROGRAM.createFragmentShader(Location.fromResource("shader/FragmentShader.fsh"));
-        SHADER_PROGRAM.link();
+        shaderProgram = new ShaderProgram();
+        shaderProgram.createVertexShader(Location.fromResource("shader/VertexShader.vsh"));
+        shaderProgram.createFragmentShader(Location.fromResource("shader/FragmentShader.fsh"));
+        shaderProgram.link();
 
-        PERSISTENT_MAPPED_VBO = new TestPersistentMappedVBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_VBO_CAPACITY);
-        PERSISTENT_MAPPED_EBO = new TestPersistentMappedEBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_EBO_CAPACITY);
-        PERSISTENT_MAPPED_IBO = new TestPersistentMappedIBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_IBO_CAPACITY);
-        PERSISTENT_MAPPED_SSBO = new TestPersistentMappedSSBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_SSBO_CAPACITY);
-        PERSISTENT_MAPPED_PBO = new TestPersistentMappedPBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_PBO_CAPACITY);
+        persistentMappedVBO = new TestPersistentMappedVBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_VBO_CAPACITY);
+        persistentMappedEBO = new TestPersistentMappedEBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_EBO_CAPACITY);
+        persistentMappedIBO = new TestPersistentMappedIBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_IBO_CAPACITY);
+        persistentMappedSSBO = new TestPersistentMappedSSBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_SSBO_CAPACITY);
+        persistentMappedPBO = new TestPersistentMappedPBO(MeshConstants.DEFAULT_FLAGS_HINT, LFJG_RENDER_CONTEXT_INITIAL_PBO_CAPACITY);
 
-        MESH = TestMesh.createMesh(
+        mesh = TestMesh.createMesh(
                 LFJG_RENDER_CONTEXT_INITIAL_VBO_CAPACITY,
                 LFJG_RENDER_CONTEXT_INITIAL_EBO_CAPACITY,
                 LFJG_RENDER_CONTEXT_INITIAL_IBO_CAPACITY
         );
 
 
-        VAO_RENDERING = new VAORendering();
+        vaoRendering = new VAORendering();
 
-        TEXTURE_CACHE = TextureCache.createTextureCache();
+        textureCache = TextureCache.createTextureCache();
     }
 
     public static void update() {
