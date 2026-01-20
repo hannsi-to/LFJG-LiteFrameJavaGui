ac305ca49cf92189d2e3cf028aaf83ee1f484844
hannsi-to <hannsi.to2@gmail.com>
2026-01-21 05:15:06 +0900

    MeshBuilderException を追加

---
+++ render/src/main/java/me/hannsi/lfjg/render/debug/exceptions/render/mesh/MeshBuilderException.java
@@ -0,0 +1,7 @@
+package me.hannsi.lfjg.render.debug.exceptions.render.mesh;
+
+public class MeshBuilderException extends RuntimeException {
+    public MeshBuilderException(String message) {
+        super(message);
+    }
+}
