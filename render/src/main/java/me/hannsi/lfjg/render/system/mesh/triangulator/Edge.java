5f1326e5db918f17f12edb2adeb220b5f752c012
hannsi-to <hannsi.to2@gmail.com>
2026-03-20 05:07:35 +0900

    Edge を追加

---
+++ render/src/main/java/me/hannsi/lfjg/render/system/mesh/triangulator/Edge.java
@@ -0,0 +1,29 @@
+package me.hannsi.lfjg.render.system.mesh.triangulator;
+
+import me.hannsi.lfjg.render.system.mesh.Vertex;
+
+import java.util.Objects;
+
+public class Edge {
+    public Vertex a;
+    public Vertex b;
+
+    public Edge(Vertex a, Vertex b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Edge edge)) {
+            return false;
+        }
+
+        return (a == edge.a && b == edge.b) || (a == edge.b && b == edge.a);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(System.identityHashCode(a) + System.identityHashCode(b));
+    }
+}
