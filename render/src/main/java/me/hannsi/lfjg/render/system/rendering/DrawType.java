088b930037e38c32855a46f139f10729748c0d5c
hannsi-to <hannsi.to2@gmail.com>
2026-03-20 05:06:27 +0900

    DrawType を修正

--- render/src/main/java/me/hannsi/lfjg/render/system/rendering/DrawType.java
+++ render/src/main/java/me/hannsi/lfjg/render/system/rendering/DrawType.java
@@ -5,23 +5,22 @@ import me.hannsi.lfjg.core.utils.type.system.IEnumTypeBase;
 import static org.lwjgl.opengl.GL11.*;
 
 public enum DrawType implements IEnumTypeBase {
-    POINTS(GL_POINTS, "Points", 1),
-    LINES(GL_LINES, "Lines", -1),
-    LINE_STRIP(GL_LINE_STRIP, "LineStrip", -1),
-    LINE_LOOP(GL_LINE_LOOP, "LineLoop", -1),
-    TRIANGLES(GL_TRIANGLES, "Triangles", 3),
-    TRIANGLE_FAN(GL_TRIANGLE_FAN, "TriangleFan", 3),
-    QUADS(GL_TRIANGLES, "Quads", 4),
-    POLYGON(GL_TRIANGLES, "Polygon", -1);
+    POINTS(GL_POINTS, "Points"),
+    LINES(GL_LINES, "Lines"),
+    LINE_STRIP(GL_LINE_STRIP, "LineStrip"),
+    LINE_LOOP(GL_LINE_LOOP, "LineLoop"),
+    TRIANGLES(GL_TRIANGLES, "Triangles"),
+    TRIANGLE_STRIP(GL_TRIANGLE_STRIP, "TriangleStrip"),
+    TRIANGLE_FAN(GL_TRIANGLE_FAN, "TriangleFan"),
+    QUADS(GL_TRIANGLES, "Quads"),
+    POLYGON(GL_TRIANGLES, "Polygon");
 
     final int id;
     final String name;
-    final int vertices;
 
-    DrawType(int id, String name, int vertices) {
+    DrawType(int id, String name) {
         this.id = id;
         this.name = name;
-        this.vertices = vertices;
     }
 
     @Override
@@ -32,9 +31,5 @@ public enum DrawType implements IEnumTypeBase {
     @Override
     public String getName() {
         return name;
-    }
-
-    public int getVertices() {
-        return vertices;
     }
 }
