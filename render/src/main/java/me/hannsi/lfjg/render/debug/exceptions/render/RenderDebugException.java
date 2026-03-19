67c3e07296b1e1bd3fb806a30f30c636fbd36a29
hannsi-to <hannsi.to2@gmail.com>
2026-03-20 05:17:34 +0900

    RenderDebugException を追加

---
+++ render/src/main/java/me/hannsi/lfjg/render/debug/exceptions/render/RenderDebugException.java
@@ -0,0 +1,7 @@
+package me.hannsi.lfjg.render.debug.exceptions.render;
+
+public class RenderDebugException extends RuntimeException {
+    public RenderDebugException(String message) {
+        super(message);
+    }
+}
