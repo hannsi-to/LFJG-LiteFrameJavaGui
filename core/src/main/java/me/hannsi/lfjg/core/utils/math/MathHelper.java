5be74fcf84427057e4bed0520a8f805d5c6a2995
hannsi-to <hannsi.to2@gmail.com>
2026-03-20 05:16:15 +0900

    MathHelper を修正

--- core/src/main/java/me/hannsi/lfjg/core/utils/math/MathHelper.java
+++ core/src/main/java/me/hannsi/lfjg/core/utils/math/MathHelper.java
@@ -754,10 +754,10 @@ public class MathHelper {
     }
 
     public static Color lerpColor(Color c1, Color c2, float t) {
-        int r = (int) (c1.getRed() * (1 - t) + c2.getRed() * t);
-        int g = (int) (c1.getGreen() * (1 - t) + c2.getGreen() * t);
-        int b = (int) (c1.getBlue() * (1 - t) + c2.getBlue() * t);
-        int a = (int) (c1.getAlpha() * (1 - t) + c2.getAlpha() * t);
+        int r = (int) (c1.getRed() + (c2.getRed() - c1.getRed()) * t);
+        int g = (int) (c1.getGreen() + (c2.getGreen() - c1.getGreen()) * t);
+        int b = (int) (c1.getBlue() + (c2.getBlue() - c1.getBlue()) * t);
+        int a = (int) (c1.getAlpha() + (c2.getAlpha() - c1.getAlpha()) * t);
 
         return new Color(r, g, b, a);
     }
