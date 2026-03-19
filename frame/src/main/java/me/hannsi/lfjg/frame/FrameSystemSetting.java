a1b7154b9abdb76e6145496f9ead9267dd08eac0
hannsi-to <hannsi.to2@gmail.com>
2026-03-20 05:10:18 +0900

    FrameSystemSetting に設定を追加

--- frame/src/main/java/me/hannsi/lfjg/frame/FrameSystemSetting.java
+++ frame/src/main/java/me/hannsi/lfjg/frame/FrameSystemSetting.java
@@ -1,4 +1,11 @@
 package me.hannsi.lfjg.frame;
 
+import static me.hannsi.lfjg.core.CoreSystemSetting.DEBUG_RUN;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_COMPAT_PROFILE;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_CORE_PROFILE;
+
 public class FrameSystemSetting {
+    public static int FRAME_GLFW_CONTEXT_VERSION_MAJOR = 4;
+    public static int FRAME_GLFW_CONTEXT_VERSION_MINOR = 6;
+    public static int FRAME_GLFW_OPENGL_PROFILE = DEBUG_RUN ? GLFW_OPENGL_COMPAT_PROFILE : GLFW_OPENGL_CORE_PROFILE;
 }
